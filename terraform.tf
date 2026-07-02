c4a750508fab27fd1468d96970867a1ec91f250d
dilipsun <dilip0527@gmail.com>
2026-07-02 15:32:39 +0100

    Add initial Terraform configuration for Azure

---
+++ terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Example: Create a Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "rg-test-terraform"
+  location = "East US"
+}
